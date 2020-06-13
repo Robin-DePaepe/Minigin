667772e3069bceb4bdeaf9d1568b7571aba76b5c
Lowskii <Lowskii@users.noreply.github.com>
2020-06-13 23:19:55 +0200

    get size function

--- Minigin/Texture2D.cpp
+++ Minigin/Texture2D.cpp
@@ -7,6 +7,18 @@ Texture2D::~Texture2D()
 	SDL_DestroyTexture(m_Texture);
 }
 
+glm::vec2 Texture2D::GetSize() const
+{
+	glm::vec2 size{};
+
+	if (m_Texture == nullptr) return size;
+
+	int w, h;
+	SDL_QueryTexture(m_Texture, nullptr, nullptr, &w, &h);
+
+	return size;
+}
+
 SDL_Texture* Texture2D::GetSDLTexture() const
 {
 	return m_Texture;
