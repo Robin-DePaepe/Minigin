c13c951580ef1e4e8f528a359b9e7d4b3e5036aa
Lowskii <Lowskii@users.noreply.github.com>
2020-06-14 15:54:57 +0200

    Player
    made a player object prefab

--- BubbleBobble/Source/Player.cpp
+++ BubbleBobble/Source/Player.cpp
@@ -1,6 +1,75 @@
 #include "Player.h"
+#include "TransformComponent.h"
+#include "BoxCollider.h"
+#include "RigidBodyComponent.h"
+#include "MovementControllerComponent.h"
+#include "SceneManager.h"
+#include "Bubble.h"
 
-//Player::Player(const wstring& name)
-//	:GameObject{name}
-//{
-//}
+Player::Player(const glm::vec2& pos, int moveLeftButton, minigin::ControllerButton moveLeftButtonC, int moveRightButton, minigin::ControllerButton moveRightButtonC, 
+	int jumpButton, minigin::ControllerButton jumpButtonC, int fireButton, minigin::ControllerButton fireButtonC, const wstring& name)
+	:GameObject{ name }
+	, m_Lives{ 4 }
+{
+	//actions
+	shared_ptr<minigin::Command> fireCommand = make_shared<FireCommand>(FireCommand{ this, &Player::FireBubble });
+	m_FireId = minigin::InputManager::GetInstance().AddInput(minigin::InputAction(fireCommand, true, fireButton, fireButtonC, minigin::InputTriggerState::Pressed));
+
+	//sprite
+	SetTexture("Sprites/Player.png");
+	GetTransfrom()->SetPosition(pos.x, pos.y);
+	GetTransfrom()->SetScale(2.f, 2.f);
+
+	//box collider non trigger
+	AddComponent(make_shared<minigin::BoxCollider>(GetTextureSize().x, GetTextureSize().y, false, false));
+
+	//box collider trigger
+	AddComponent(make_shared<minigin::BoxCollider>(GetTextureSize().x, GetTextureSize().y, true, false));
+
+	//add rigid body
+	AddComponent(make_shared<minigin::RigidBodyComponent>());
+
+	//add movement
+	m_spMovement = make_shared<MovementControllerComponent>(25.f, 50.f, moveLeftButton, moveRightButton,jumpButton, moveLeftButtonC, moveRightButtonC, jumpButtonC);
+	AddComponent(m_spMovement);
+}
+
+void Player::SetActions(bool active)
+{
+	GameObject::SetActions(active);
+
+	minigin::InputManager::GetInstance().ChangeInputActionStatus(m_FireId, active);
+}
+
+void Player::OnTriggerStay(GameObject* other)
+{
+	if (other->GetName() == L"Enemy")
+	{
+		--m_Lives;
+
+		if (m_Lives == 0) minigin::SceneManager::GetInstance().SetActiveGameScene(minigin::SceneManager::GetInstance().GetScene(L"Main Menu"));
+	}
+	if (other->GetName() == L"Pickup")
+	{
+
+	}
+}
+
+shared_ptr<minigin::GameObject> Player::GetBubble()
+{
+	auto copy = m_spBubble;
+
+	m_spBubble = nullptr;
+
+	return copy;
+}
+
+void FireCommand::Execute()
+{
+	(m_Obj->*m_pFucntion)();
+}
+
+void Player::FireBubble()
+{
+	m_spBubble = make_shared<Bubble>(GetTransfrom()->GetPosition(), m_spMovement->IsFacingLeft());
+}
--- BubbleBobble/Source/Player.h
+++ BubbleBobble/Source/Player.h
@@ -1,23 +1,53 @@
 #pragma once
-//#include "GameObject.h"
-//#include <string>
-//
-//
-//class Player final : public GameObject
-//{
-//public:
-//	//rule of 5
-//	Player(const wstring& name);
-//	~Player() = default;
-//
-//	Player(const Player& other) = delete;
-//	Player(Player&& other) = delete;
-//	Player& operator=(const Player& other) = delete;
-//	Player& operator=(Player&& other) = delete;
-//		 void SetActions(bool active) override;
-//private:
-//
-//
-//	//variables
-//};
-//
+#include <iostream> 
+#include <vector>
+#include <windows.h>
+#include "Logger.h"
+#include "GameObject.h"
+#include "Command.h"
+#include "GameObject.h"
+#include <InputManager.h>
+
+class Bubble;
+class Player;
+class MovementControllerComponent;
+
+class FireCommand final : public minigin::Command
+{
+public:
+	FireCommand(Player* pObj, void(Player::* pFucntion)())
+		:Command{}
+		, m_Obj{ pObj }
+		, m_pFucntion{ pFucntion }
+	{};
+
+	virtual void Execute()  override;
+	virtual void Undo() override {};
+private:
+	Player* m_Obj;
+	void(Player::* m_pFucntion)();
+};
+
+class Player final : public minigin::GameObject
+{
+public:
+	Player(const glm::vec2& pos, int moveLeftButton, minigin::ControllerButton moveLeftButtonC, int moveRightButton, minigin::ControllerButton moveRightButtonC
+		, int jumpButton, minigin::ControllerButton jumpButtonC, int fireButton, minigin::ControllerButton fireButtonC,const wstring& name = L"Player");
+
+	//functions
+	virtual void SetActions(bool active) override;
+	virtual void OnTriggerStay(GameObject* other) override;
+
+	shared_ptr<GameObject> GetBubble();
+
+private:
+	//datamembers
+	unsigned int m_Lives;
+	size_t m_FireId;
+
+	shared_ptr<MovementControllerComponent> m_spMovement;
+	shared_ptr<Bubble> m_spBubble;
+	//functions
+	void FireBubble();
+};
+
